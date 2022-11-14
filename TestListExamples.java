4cd33cbb43aa312dbf4a880a575157b2ed63317c
jadontran <114566278+jadontran@users.noreply.github.com>
2022-11-14 10:28:58 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,33 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TestListExamples {
   // Write your grading tests here!
+
+  class Checker implements StringChecker {
+ 	 boolean checkString(String s) { 
+	 	if ((s.contains("A")) || (s.contains("a"))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+  } 
+  @Test
+  public void filterTest() {
+    List<String> test = new ArrayList<>();
+
+  }
+
+  @Test
+  public void mergeTest() {
+    List<String> test = Arrays.asList();
+
+
+  }
+
+}
 }
